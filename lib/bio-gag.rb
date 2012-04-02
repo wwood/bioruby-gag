42816ff06f28ca87fffa900e6c83c47c7cbe3477
Ben J Woodcroft <gmail.com after donttrustben>
2012-04-02 18:48:11 +1000

    point to the no_debug pileup_iterator

--- lib/bio-gag.rb
+++ lib/bio-gag.rb
@@ -2,7 +2,7 @@
 require 'bio-logger'
 Bio::Log::LoggerPlus.new('bio-gag')
 
-$:.unshift File.join(File.dirname(__FILE__),'../../bioruby-pileup_iterator/lib/')
+$:.unshift File.join(File.dirname(__FILE__),'../../bioruby-pileup_iterator_no_debug/lib/')
 require 'bio-pileup_iterator'
 require 'bio/db/gag'
 
